df5f7bbba7bad7188a95c2ef3073394e64396a06
t_shimomura <t_shimomura@septeni-original.co.jp>
2019-05-22 22:14:14 +0900

    modify Cont stop test

--- src/main/scala/com/s10myk4/domain/lifcycle/WarriorRepository.scala
+++ src/main/scala/com/s10myk4/domain/lifcycle/WarriorRepository.scala
@@ -4,8 +4,7 @@ import com.s10myk4.domain.model.character.warrior.{Warrior, WarriorId}
 
 import scala.language.higherKinds
 
-trait WarriorRepository[F[_]] extends BaseRepository[F] with ResolveFeatureRepository[F] {
-
-  type ID = WarriorId
-  type E  = Warrior
+trait WarriorRepository[F[_]] {
+  def update(warrior: Warrior): F[Unit]
+  def resolveBy(id: WarriorId): F[Option[Warrior]]
 }
